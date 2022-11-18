ae10aab8940dd866f851b6a2e1588903e3626a40
Benjamin Siret <benjaminsiret@outlook.fr>
2022-11-18 11:06:32 +0100

    add handleClickIconEye test

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,23 +1,24 @@
 /**
  * @jest-environment jsdom
  */
-
 import { screen, waitFor } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
 import { ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
-
+import Bills from "../containers/Bills.js";
 import router from "../app/Router.js";
+import userEvent from "@testing-library/user-event";
+
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
-
       Object.defineProperty(window, 'localStorage', { value: localStorageMock });
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
       }));
+
       const root = document.createElement("div");
       root.setAttribute("id", "root");
       document.body.append(root);
@@ -25,7 +26,6 @@ describe("Given I am connected as an employee", () => {
       window.onNavigate(ROUTES_PATH.Bills);
       await waitFor(() => screen.getByTestId('icon-window'));
       const windowIcon = screen.getByTestId('icon-window');
-      //to-do write expect expression
       expect(windowIcon.className).toBe("active-icon");
     });
     test("Then bills should be ordered from earliest to latest", () => {
@@ -36,4 +36,25 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted);
     });
   });
+
+  describe("When I click on the icon eye", () => {
+    test("A modal should opened", () => {
+      const handleClickIconEye = jest.fn(Bills.handleClickIconEye);
+      const eye = screen.getAllByTestId("icon-eye")[0];
+      eye.addEventListener('click', handleClickIconEye);
+      userEvent.click(eye);
+      expect(handleClickIconEye).toHaveBeenCalled();
+
+      const modale = screen.getByTestId("modaleFile");
+      expect(modale).toBeTruthy();
+    });
+  });
+
+  // TODO: describe => quand je suis sur la page Bills, test => quand je clique sur le bouton
+  //      "nouvelle note de frais", je suis redirigé vers la page Bills/New
+
+
+
+  // TODO: describe => quand je suis sur la page Bills, test => quand je clique sur le bouton de déconnexion,
+  //       je suis déconnecté et redirigé vers la page login
 });
